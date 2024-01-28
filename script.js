0cdcd7dfe3a8d07d5e9daf4e50ec3eecb31c2035
genocider34 <alonzoball07@gmail.com>
2024-01-28 10:19:37 +0800

    refactoring

--- script.js
+++ script.js
@@ -4,24 +4,37 @@ const apiData = {
   id: 25,
 };
 
-const { url, type, id } = apiData;
-const apiURL = `${url}${type}/${id}`;
+const { url, type } = apiData;
+const apiURL = `${url}${type}/`;
 
-async function generateHtml(url) {
+const ids = function (id) {
+  return id;
+};
+
+async function generateHtml(url, id) {
   try {
-    const res = await axios.get(url);
+    const res = await axios.get(`${url}${id}`);
     const data = res.data;
     const html = `
         <div class="name">${data.name}</div>
         <img src="${data.sprites.front_default}"/>
-
     `;
 
-    const pokemonDiv = document.querySelector(".pokemon");
+    const elements = {
+      body: document.querySelector("body"),
+      pokemonDiv: document.createElement("DIV"),
+    };
+    const { body, pokemonDiv } = elements;
+
+    pokemonDiv.className = "pokemon";
+    body.appendChild(pokemonDiv);
     pokemonDiv.innerHTML = html;
   } catch (e) {
     console.log("error");
   }
 }
 
-generateHtml(apiURL);
+generateHtml(apiURL, 25);
+generateHtml(apiURL, 40);
+generateHtml(apiURL, 523);
+generateHtml(apiURL, 619);
